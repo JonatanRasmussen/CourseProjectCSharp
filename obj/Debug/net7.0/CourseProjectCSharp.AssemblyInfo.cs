fa3257d22885cecf19b9791594af263df2034d6d
JonatanRasmussen <115923496+JonatanRasmussen@users.noreply.github.com>
2024-01-27 00:00:20 +0100

    Yet another test commit

--- obj/Debug/net7.0/CourseProjectCSharp.AssemblyInfo.cs
+++ obj/Debug/net7.0/CourseProjectCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseProjectCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf1f8f8b108228af0348859c7ef83c07f5ef5b6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710e355e6aaf7bb38f8847c8393b8c5666d77b24")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseProjectCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseProjectCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
